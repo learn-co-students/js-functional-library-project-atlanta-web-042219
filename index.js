3023f5ca9b393667d2fa7a787ca13591a0c61fcc
ascotttoney <ascotttoney@gmail.com>
2019-06-12 15:10:50 -0400

    Done.

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(array, duh) {
+      let newCollection = []
+      for (let i in array) newCollection.push(array[i])
+      for (let x = 0; x < newCollection.length; x++) duh(newCollection[x])
+      return array;
     },
 
-    map: function() {
-
+    map: function(array, duh) {
+      let newCollection = []
+      for (let i in array) newCollection.push(array[i])
+      for (let x = 0; x < newCollection.length; x++)
+        newCollection[x] = duh(newCollection[x])
+      return newCollection;
     },
 
-    reduce: function() {
+    reduce: function(array, callback, acc = 0) {
 
     },
 
